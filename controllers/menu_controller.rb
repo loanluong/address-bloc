be490eb11c7ef3cf4d501bddf021f0ed1fe01ddf
Loan Luong <loanlu@gmail.com>
2017-02-20 14:25:24 -0800

    view by entry feature added

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View entry number"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -35,6 +36,10 @@ class MenuController
         read_csv
         main_menu
       when 5
+        system "clear"
+        view_entry_number
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -79,6 +84,22 @@ class MenuController
 
   end
 
+  def view_entry_number
+    system "clear"
+    puts "Enter entry number"
+    number = gets.chomp.to_i
+
+    if number < address_book.entries.count
+       puts address_book.entries[number]
+       puts "press enter to return to main menu"
+       gets.chomp
+       system "clear"
+    else puts "That is not a valid selection"
+      view_entry_number
+    end
+
+
+  end
   def entry_submenu(entry)
     puts "n - next entry"
     puts "d - delete entry"
