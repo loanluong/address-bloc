aeb5da07c3c3774713559f9311b8c963403b51c2
Loan Luong <loanlu@gmail.com>
2017-02-23 16:21:13 -0800

    added iterative search

--- models/address_book.rb
+++ models/address_book.rb
@@ -48,4 +48,13 @@ require "csv"
 
     return nil
    end
+
+   def iterative_search(name)
+     entries.each do |entry|
+     if name == entry.name
+       return entry
+     end
+   end
+   return nil
  end
+end
